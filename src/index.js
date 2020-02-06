be6c1b1a26691011b25759b5340fb29926544aaa
Tyler <tyler@tylerewillis.com>
2020-02-05 21:06:43 -0500

    added functionality for YYYY-MM-DD format

--- src/index.js
+++ src/index.js
@@ -37,16 +37,34 @@ function getRange(template, start, end) {
   return range
 }
 
+const isFourTwoFour = (date) => {
+  if (!date.length == 10) return false
+  const divider = date.charAt(4)
+  const array = date.split(divider)
+  if (array.length == 3) {
+    if (array[0].length == 4 && array[1].length == 2 && array[2].length == 2) {
+      var newArray = [array[1], array[2], array[0]]
+      return newArray.join('-')
+    } else {
+      return false
+    }
+  }
+  return false
+}
+
 function date(template, s = null, e = null){
 
   if (e) {
-    const start = new Date(s)
-    const end = new Date(e)
+    const start = (isFourTwoFour(s)) ? new Date(isFourTwoFour(s)) : new Date(s)
+    const end = (isFourTwoFour(e)) ? new Date(isFourTwoFour(e)) : new Date(e)
     return getRange(template, start, end)
   } else {
     var range = ''
-    var text = false
-    const start = (s) ? new Date(s) : new Date();
+    var text = false;
+    var start = ''
+    if (s) {
+      start = (isFourTwoFour(s)) ? new Date(isFourTwoFour(s)) : new Date(s)
+    } else start = new Date();
     [...template].forEach((c) => {
       if (c == '[') text = true
       else if (c == ']') text = false
